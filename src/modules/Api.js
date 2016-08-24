ccf3f9a6f69f35850fababec93dac603be65e95d
Conner Frey <connerfrey@Conners-MacBook-Pro-2.local>
2016-08-24 01:48:54 -0700

    last API

--- src/modules/Api.js
+++ src/modules/Api.js
@@ -76,7 +76,7 @@ function chooseUrl(routeType){
 
 var Api = module.exports = {
   server: new Route(""),
-  db: new Route(window.location.port == '4000' ? 'http://localhost:1337' : 'https://quizzly-frontend-prod.herokuapp.com'),
+  db: new Route(window.location.port == '4000' ? 'http://localhost:1337' : 'https://quizzly-backend-prod.herokuapp.com'),
   otherDb: new Route(chooseUrl("redPay")),
-  baseUrl: window.location.port == '4000' ? 'http://localhost:1337' : 'https://quizzly-frontend-prod.herokuapp.com',
+  baseUrl: window.location.port == '4000' ? 'http://localhost:1337' : 'https://quizzly-backend-prod.herokuapp.com',
 }
